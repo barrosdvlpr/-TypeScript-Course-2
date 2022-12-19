b30748668513d496e8434556bb498d5ca5cb2541
barrosdvlpr <barrosdvlpr@gmail.com>
2022-12-19 08:21:40 -0300

    Create mapped types using Record type

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,3 +6,20 @@ export default function Home() {
     </div>
   )
 }
+
+// type AgesType = {
+//   [name: string]: number;
+// }
+
+type NamesType = "Jonh Doe" | "Jane Doe" | "baby Doe" | "Jannie Doe"
+
+type AgesType = Record<NamesType, number>;
+
+const ages: AgesType = {
+  "Jonh Doe": 29,
+  "Jane Doe": 25,
+  "baby Doe": 5,
+  "Jannie Doe": 50,
+}
+
+console.log(ages)
