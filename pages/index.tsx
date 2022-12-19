591658bc1db4a5bafce5169a1c6a8f8e260f866f
barrosdvlpr <barrosdvlpr@gmail.com>
2022-12-19 08:14:45 -0300

    Avoid errors using unknown type

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,3 +6,17 @@ export default function Home() {
     </div>
   )
 }
+
+type Some = {
+  something: () => void
+}
+
+function log(val: unknown) {
+  if (typeof val === "string") {
+    console.log(val.toUpperCase())
+  }
+  console.log((val as number) + 20)
+  console.log((val as Some).something())
+}
+
+log(5)
