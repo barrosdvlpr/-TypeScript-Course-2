c612e78096bec3c481fa7f7cfc2cc5569e7fa5fc
barrosdvlpr <barrosdvlpr@gmail.com>
2022-12-19 08:31:40 -0300

    Reduce duplicate types with Generic Types

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,3 +6,31 @@ export default function Home() {
     </div>
   )
 }
+
+type Box<T> = {
+  name: string;
+  content: T;
+}
+
+// type Box = {
+//   name: string;
+//   content: unknown;
+// }
+
+// type NumberBox = {
+//   name: string;
+//   content: number;
+// }
+
+const stringBox: Box<string> = {
+  name: 'String Box',
+  content: 'A string',
+}
+
+const numberBox: Box<number> = {
+  name: 'String Box',
+  content: 50,
+}
+
+console.log(stringBox.content.toLocaleUpperCase())
+console.log(numberBox.content * 5)
