be5305301b3f71fd0fd8d3776437235349bdfb78
barrosdvlpr <barrosdvlpr@gmail.com>
2022-12-19 07:29:59 -0300

    exclude properties from type

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,3 +6,21 @@ export default function Home() {
     </div>
   )
 }
+
+type Item = {
+  name: string;
+  description: string;
+  price: number;
+  currency: string;
+}
+
+type PricelessItem = Omit<Item, "price" | "currency">
+
+const item: PricelessItem = {
+  name: "laptop Bag",
+  description: "Leather Bag for laptop",
+  // price: 34,
+  // currency: "USD",
+}
+
+console.log(item);
