2ac0f1e17fd4254c7c1c00dc47091b73f330ba0f
barrosdvlpr <barrosdvlpr@gmail.com>
2022-12-19 07:42:59 -0300

    Create Immutable Types in TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,3 +6,33 @@ export default function Home() {
     </div>
   )
 }
+
+type User = {
+  readonly name: string;
+  readonly age: number;
+  readonly education: {
+    readonly degree: string
+  }
+  skills: ReadonlyArray<string>;
+}
+
+const user = {
+  name: 'John Doe',
+  age: 26,
+  education: {
+    degree: "SE"
+  },
+  skills: ["JavaScript", "TypeScript"]
+  
+}
+
+// user.name = 'John Doe';
+// user.age = 22;
+// user.education = {
+//   degree: "IT"
+// }
+// user.education.degree = "IT"
+// user.skills = ["Building"]
+// user.skills.push("Deploying")
+
+console.log(user)
