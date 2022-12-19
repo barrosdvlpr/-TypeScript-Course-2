181725884b23b4f525741dfebc671947f3450ca3
barrosdvlpr <barrosdvlpr@gmail.com>
2022-12-19 07:16:46 -0300

    create type with fields of another type using pick

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,3 +6,30 @@ export default function Home() {
     </div>
   )
 }
+
+type Item = {
+  name: string;
+  description: string;
+  price: number;
+  currency: string;
+  image: string;
+}
+
+type ItemPreview = Pick<Item, "name" | "image">
+
+const item: Item = {
+  name: "Macbook",
+  description: "Macbook Pro",
+  price: 3923,
+  currency: "USD",
+  image: "https://cdn.apple.com/mbpro.png"
+}
+
+const ItemPreview: ItemPreview ={
+  name: item.name,
+  image: item.image,
+}
+
+console.log(ItemPreview)
+
+console.log(item)
